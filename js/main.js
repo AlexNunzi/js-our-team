59dd1028a8d9eaec1ff792b15c0ddf6d4974b842
Alex Nunzi <alex.nunzi@hotmail.it>
2023-03-08 14:04:52 +0100

    Creazione dell'array di oggetti con i dati di ogni membro del team.

---
+++ js/main.js
@@ -0,0 +1,43 @@
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+
+const membersList= [
+    {
+        'nome completo': 'Wayne Barnett',
+        occupazione: 'Founder & CEO',
+        'immagine del profilo': 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        'nome completo': 'Angela Caroll',
+        occupazione: 'Chief Editor',
+        'immagine del profilo': 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        'nome completo': 'Walter Gordon',
+        occupazione: 'Office Manager',
+        'immagine del profilo': 'walter-gordon-office-manager.jpg'
+    },
+    {
+        'nome completo': 'Angela Lopez',
+        occupazione: 'Social Media Manager',
+        'immagine del profilo': 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        'nome completo': 'Scott Estrada',
+        occupazione: 'Developer',
+        'immagine del profilo': 'scott-estrada-developer.jpg'
+    },
+    {
+        'nome completo': 'Barbara Ramos',
+        occupazione: 'Graphic Designer',
+        'immagine del profilo': 'barbara-ramos-graphic-designer.jpg'
+    }
+]
+
+console.log("Larray di oggetti creato è:", membersList);
+
